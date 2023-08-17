d7113c612bc693101314ad6aadf57c1229efb57b
Rathod Yuvraj <rathodyuvrajsinh2004@gmail.com>
2023-08-17 10:34:47 +0530

    Practical-4 created

--- app/src/main/java/com/rathodyuvraj/mad_practical_4_21012011129/RegisterActivity.kt
+++ app/src/main/java/com/rathodyuvraj/mad_practical_4_21012011129/RegisterActivity.kt
@@ -1,11 +1,20 @@
 package com.rathodyuvraj.mad_practical_4_21012011129
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        val Login: Button = findViewById(R.id.button)
+
+        Login.setOnClickListener {
+            Intent(this@RegisterActivity, LoginActivity::class.java).also { startActivity(it) }
+        }
+
     }
 }
